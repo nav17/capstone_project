0403dc18c055f11d6876daf21d81a529cf76e9b3
nav17 <cayendo1@gmail.com>
2025-05-11 17:36:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Capstone Project
+
+An ETL & Streamlit Project.
+
+### Dataset
+
+https://api.tfl.gov.uk/
+
+## Installation
+
+### Prerequisites
+
+Requires Python 3.13 or above
+
+### Setup
+
+To setup this project:
+
+```
+pip install -r requirements-setup.txt
+pip install -e .
+```
+
+
+To run (during development):
+
+```
+python3 -m scripts.run_etl dev
+```
+
+### Troubleshooting
+
+TBD
---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+# Read the requirements from the requirements.txt file
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name="nav_capstone_project",
+    version="0.1.0",
+    description=("An ETL project for extracting, transforming, and loading data."),
+    author="Navdip Narotra",
+    author_email="navdip.n@gmail.com",
+    url="https://github.com/nav17/capstone_project",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "run_etl=scripts.run_etl:main",
+            "run_tests=tests.run_tests:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
+
+# Explanation - note that pip install setuptools is required to run this file
+# packages: Uses find_packages() to automatically find all packages in your project.  # noqa: E501
+# include_package_data: Includes additional files specified in MANIFEST.in.
+# install_requires: A list of dependencies required for your project.
+# entry_points: Defines command-line scripts that can be run.
+# run_etl=scripts.run_etl:main: Allows you to run the ETL pipeline using the run_etl command.  # noqa: E501
+# classifiers: Metadata about your project, such as the programming language, license, and operating system compatibility.  # noqa: E501
+# python_requires: Specifies the required Python version.
+#
+# Using this file:
+# Run the command
+# pip install .
+#
+# This will install your project and its dependencies in your Python environment.  # noqa: E501
+# It creates a build folder that contains the compiled version of your project.
+# You can then run the run_etl command to execute your ETL pipeline:
+# python -m etl_example.run_etl
